c5d33b007eeb03231282940fab35b4a1504c59c6
vg_dev <vincenzo.gandolfo7@gmail.com>
2023-02-06 16:57:15 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/my/vgdev/slivergrid/MainActivity.kt
@@ -0,0 +1,6 @@
+package my.vgdev.slivergrid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('SliverGrid'),
+        ),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 2,
+              mainAxisSpacing: 2,
+              childAspectRatio: 2 / 1,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => Container(
+                color: Colors.blue,
+              ),
+              childCount: 4,
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(20),
+            sliver: SliverGrid(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => Container(
+                  color: Colors.blue,
+                ),
+                childCount: 4,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
